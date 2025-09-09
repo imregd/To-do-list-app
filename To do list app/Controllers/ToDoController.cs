a46dc4ebb17d78cf9bc12d4d0b4058d41916abea
aleks <aleks@DESKTOP-G6OPQHT>
2025-09-09 20:18:13 +0100

    e

---
+++ To do list app/Controllers/ToDoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using to_do_list_app.Models;
+
+namespace to_do_list_app.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public static List<ToDoListModel> ToDoItems = new List<ToDoListModel>();
+
+        public IActionResult Index()
+        {
+            return View(ToDoItems);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ToDoListModel item)
+        {
+            Guid id = Guid.NewGuid();
+            item.Id = id;
+
+            ToDoItems.Add(item);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ To do list app/Models/ToDoListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace to_do_list_app.Models
+{
+    public class ToDoListModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int Priority { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
